12bc892f0db598cadde9413d4604d99bf4d3680c
Jason D'Oyley <jdfdoyley@gmail.com>
2021-08-04 07:18:44 -0400

    Integer tutorials

---
+++ integers.py
@@ -0,0 +1,27 @@
+# Following numbers are integers
+x = 10
+y = -10
+z = 123456789
+print(x, y, z)
+
+
+# Integers have unlimited precision
+w = 999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999
+print(w)
+
+# Integers in binary, octual and hexadecimal formats
+
+# binary
+print(0b10111011)   # 187
+
+# octal
+print(0o10)         # 8
+
+# hex
+print(0xFF)         # 255
+
+# Boolean in Python
+# Boolean is a sub-type of integers
+x = True
+y = False
+
