3aaed46513dff564dd03c66b3fd2b2604a123e14
Jason D'Oyley <jdfdoyley@gmail.com>
2021-08-04 07:29:25 -0400

    Floats tutorial

---
+++ floats.py
@@ -0,0 +1,18 @@
+# Following numbers are floats
+x = 10.1
+y = -10.5
+z = 1.123456
+
+
+# Scientific notation
+print(42e3)         # 42000.0
+print(4.2e-3)       # 0.0042
+
+# Maximum value of a float
+print(1.79e308)     # 1.79e+308
+print(1.8e308)      # inf
+
+
+# Minimum value of a float
+print(5e-324)       # 5e-324
+print(5e-325)       # 0.0
