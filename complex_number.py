bc51f55c8d0d676f82dbf6010f32106cc3a47183
Jason D'Oyley <jdfdoyley@gmail.com>
2021-08-04 07:34:11 -0400

    Complex numbers tutorial

---
+++ complex_number.py
@@ -0,0 +1,18 @@
+# Complex Numbers
+# A complex number is specified as real_part + imaginary_part, where the
+# imaginary_part is written with a j or J
+
+# Following numbers are complex numbers
+x = 2j
+y = 3+4j
+
+# To extract real and imaginary parts from a complex number x, use x.real and
+# x.imag
+
+x = 3+4j
+
+# real part
+print(x.real)       # 3.0
+
+# imaginary part
+print(x.imag)       # 4.0
